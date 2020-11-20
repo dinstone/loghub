89a8a70212d9ebb8016fc636f232a67c5c50d593
dinstone <dinstone@163.com>
2020-11-20 20:10:50 +0800

    release 1.3.0; supprot javadoc and source jar

--- loghub-log4j/src/main/java/com/dinstone/loghub/log4j/Log4jLogDelegate.java
+++ loghub-log4j/src/main/java/com/dinstone/loghub/log4j/Log4jLogDelegate.java
@@ -27,149 +27,145 @@ import com.dinstone.loghub.spi.LogDelegate;
  */
 public class Log4jLogDelegate implements LogDelegate {
 
-	private static final String FQCN = com.dinstone.loghub.Logger.class.getCanonicalName();
+    private static final String FQCN = com.dinstone.loghub.Logger.class.getCanonicalName();
 
-	private final org.apache.log4j.Logger logger;
+    private final org.apache.log4j.Logger logger;
 
-	Log4jLogDelegate(final String name) {
-		logger = org.apache.log4j.Logger.getLogger(name);
-	}
+    Log4jLogDelegate(final String name) {
+        logger = org.apache.log4j.Logger.getLogger(name);
+    }
 
-	@Override
-	public boolean isInfoEnabled() {
-		return logger.isInfoEnabled();
-	}
+    @Override
+    public boolean isInfoEnabled() {
+        return logger.isInfoEnabled();
+    }
 
-	@Override
-	public boolean isDebugEnabled() {
-		return logger.isDebugEnabled();
-	}
+    @Override
+    public boolean isDebugEnabled() {
+        return logger.isDebugEnabled();
+    }
 
-	@Override
-	public boolean isTraceEnabled() {
-		return logger.isTraceEnabled();
-	}
+    @Override
+    public boolean isTraceEnabled() {
+        return logger.isTraceEnabled();
+    }
 
-	@Override
-	public void fatal(final Object message) {
-		log(Level.FATAL, message);
-	}
+    @Override
+    public void fatal(final Object message) {
+        log(Level.FATAL, message);
+    }
 
-	@Override
-	public void fatal(final Object message, final Throwable t) {
-		log(Level.FATAL, message, t);
-	}
+    @Override
+    public void fatal(final Object message, final Throwable t) {
+        log(Level.FATAL, message, t);
+    }
 
-	@Override
-	public void error(final Object message) {
-		log(Level.ERROR, message);
-	}
+    @Override
+    public void error(final Object message) {
+        log(Level.ERROR, message);
+    }
 
-	@Override
-	public void error(Object message, Object... params) {
-		throwUnsupportedOperationException();
-	}
+    @Override
+    public void error(Object message, Object... params) {
+        log(Level.ERROR, message);
+    }
 
-	@Override
-	public void error(final Object message, final Throwable t) {
-		log(Level.ERROR, message, t);
-	}
+    @Override
+    public void error(final Object message, final Throwable t) {
+        log(Level.ERROR, message, t);
+    }
 
-	@Override
-	public void error(Object message, Throwable t, Object... params) {
-		throwUnsupportedOperationException();
-	}
+    @Override
+    public void error(Object message, Throwable t, Object... params) {
+        log(Level.ERROR, message, t);
+    }
 
-	@Override
-	public void warn(final Object message) {
-		log(Level.WARN, message);
-	}
+    @Override
+    public void warn(final Object message) {
+        log(Level.WARN, message);
+    }
 
-	@Override
-	public void warn(Object message, Object... params) {
-		throwUnsupportedOperationException();
-	}
+    @Override
+    public void warn(Object message, Object... params) {
+        log(Level.WARN, message);
+    }
 
-	@Override
-	public void warn(final Object message, final Throwable t) {
-		log(Level.WARN, message, t);
-	}
+    @Override
+    public void warn(final Object message, final Throwable t) {
+        log(Level.WARN, message, t);
+    }
 
-	@Override
-	public void warn(Object message, Throwable t, Object... params) {
-		throwUnsupportedOperationException();
-	}
+    @Override
+    public void warn(Object message, Throwable t, Object... params) {
+        log(Level.WARN, message, t);
+    }
 
-	@Override
-	public void info(final Object message) {
-		log(Level.INFO, message);
-	}
+    @Override
+    public void info(final Object message) {
+        log(Level.INFO, message);
+    }
 
-	@Override
-	public void info(Object message, Object... params) {
-		throwUnsupportedOperationException();
-	}
+    @Override
+    public void info(Object message, Object... params) {
+        log(Level.INFO, message);
+    }
 
-	@Override
-	public void info(final Object message, final Throwable t) {
-		log(Level.INFO, message, t);
-	}
+    @Override
+    public void info(final Object message, final Throwable t) {
+        log(Level.INFO, message, t);
+    }
 
-	@Override
-	public void info(Object message, Throwable t, Object... params) {
-		throwUnsupportedOperationException();
-	}
+    @Override
+    public void info(Object message, Throwable t, Object... params) {
+        log(Level.INFO, message, t);
+    }
 
-	@Override
-	public void debug(final Object message) {
-		log(Level.DEBUG, message);
-	}
+    @Override
+    public void debug(final Object message) {
+        log(Level.DEBUG, message);
+    }
 
-	@Override
-	public void debug(Object message, Object... params) {
-		throwUnsupportedOperationException();
-	}
+    @Override
+    public void debug(Object message, Object... params) {
+        log(Level.DEBUG, message);
+    }
 
-	@Override
-	public void debug(final Object message, final Throwable t) {
-		log(Level.DEBUG, message, t);
-	}
+    @Override
+    public void debug(final Object message, final Throwable t) {
+        log(Level.DEBUG, message, t);
+    }
 
-	@Override
-	public void debug(Object message, Throwable t, Object... params) {
-		throwUnsupportedOperationException();
-	}
+    @Override
+    public void debug(Object message, Throwable t, Object... params) {
+        log(Level.DEBUG, message, t);
+    }
 
-	@Override
-	public void trace(final Object message) {
-		log(Level.TRACE, message);
-	}
+    @Override
+    public void trace(final Object message) {
+        log(Level.TRACE, message);
+    }
 
-	@Override
-	public void trace(Object message, Object... params) {
-		throwUnsupportedOperationException();
-	}
+    @Override
+    public void trace(Object message, Object... params) {
+        log(Level.TRACE, message);
+    }
 
-	@Override
-	public void trace(final Object message, final Throwable t) {
-		log(Level.TRACE, message, t);
-	}
+    @Override
+    public void trace(final Object message, final Throwable t) {
+        log(Level.TRACE, message, t);
+    }
 
-	@Override
-	public void trace(Object message, Throwable t, Object... params) {
-		throwUnsupportedOperationException();
-	}
+    @Override
+    public void trace(Object message, Throwable t, Object... params) {
+        log(Level.TRACE, message, t);
+    }
 
-	private void log(Level level, Object message) {
-		log(level, message, null);
-	}
+    private void log(Level level, Object message) {
+        log(level, message, null);
+    }
 
-	private void log(Level level, Object message, Throwable t) {
-		logger.log(FQCN, level, message, t);
-	}
-
-	private void throwUnsupportedOperationException() {
-		throw new UnsupportedOperationException("Log4j version used in loghub doesn't support parameterized logging.");
-	}
+    private void log(Level level, Object message, Throwable t) {
+        logger.log(FQCN, level, message, t);
+    }
 
 }
